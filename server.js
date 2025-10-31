b701d3ff6f2b53b49d81a678db0d7dc5b5653466
mwuK75 <206798259+mwuK75@users.noreply.github.com>
2025-10-31 11:56:08 +0900

    変更

--- server.js
+++ server.js
@@ -5,8 +5,28 @@ const app = new Hono();
 app.use('/*', serveStatic({ root: './public' }));
 
 // GETリクエストに対する処理
-app.get('/api', async (c) => {
+app.get('/api', (c) => {
   return c.json({ message: 'GET' });
+});
+
+// POSTリクエストに対する処理
+app.post('/api', async (c) => {
+  // POSTされたJSONボディを取得
+  const body = await c.req.json();
+  return c.json({ message: 'POST', data: body });
+});
+// PUTリクエストに対する処理
+app.put('/api', async (c) => {
+  // PUTされたJSONボディを取得
+  const body = await c.req.json();
+  return c.json({ message: 'PUT', data: body });
+});
+
+// DELETEリクエストに対する処理
+app.delete('/api', async (c) => {
+
+  const body = await c.req.json();
+  return c.json({ message: 'DELETE', data: body });
 });
 
 Deno.serve(app.fetch);
